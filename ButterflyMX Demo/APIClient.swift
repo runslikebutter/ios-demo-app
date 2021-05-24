9ee8b49c18ad8634e4e5ac1b79ad5c6ed73c7af7
Yingtao Guo <yingtao@kakueitous-MacBook-Pro.local>
2021-05-24 10:25:01 -0400

    fix: review fixes

--- ButterflyMX Demo/APIClient.swift
+++ ButterflyMX Demo/APIClient.swift
@@ -8,9 +8,9 @@
 
 import Foundation
 
-enum Result<Success, Error: Swift.Error> {
-    case success(Success)
-    case failure(Error)
+enum APIError: Error {
+    case unableToCreateRequest(message: String)
+    case runtime(error: Error)
 }
 
 class APIClient {
@@ -22,17 +22,22 @@ class APIClient {
     //TODO: Temp implementations for more registerDevice and unRegisterDevice.
     //TODO: More implementation will be added after setting up the server in docker
     
-    func registerDevice(with deviceToken: String, completion: (Result<Data, Error>) -> Void) {
-        print("Register the device with deviceToken: \(deviceToken)")
+    func registerDevice(with deviceToken: String, completion: @escaping (Result<Data, APIError>) -> Void) {
+        guard deviceToken.count > 0 else {
+            completion(.failure(.unableToCreateRequest(message: "invalid device token.")))
+            return
+        }
         
+        print("Register the device with deviceToken: \(deviceToken)")        
         UserDefaults.standard.set(deviceToken, forKey: "deviceToken")
         UserDefaults.standard.synchronize()
         
         completion(.success(Data()))
     }
     
-    func unRegisterDevice(completion: (Result<Data, Error>) -> Void) {
+    func unRegisterDevice(completion: @escaping (Result<Data, APIError>) -> Void) {
         guard let deviceToken = UserDefaults.standard.string(forKey: "deviceToken") else {
+            completion(.failure(.unableToCreateRequest(message: "invalid device token.")))
             return
         }
         
