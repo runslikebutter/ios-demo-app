b13162560fa9a0abbba194a2227717e1f518fa18
Yingtao Guo <yingtao.guo@butterflymx.com>
2021-11-18 14:50:28 -0500

    feat: add CallNotificationType setting
    add CallNotificationTypeManager
    add CallNotificationType setting UI
    refactor AccountTableViewController

--- ButterflyMX Demo/Controllers/AccountTableViewController.swift
+++ ButterflyMX Demo/Controllers/AccountTableViewController.swift
@@ -8,6 +8,7 @@
 import UIKit
 import BMXCore
 import SVProgressHUD
+
 class AccountTableViewController: UITableViewController {
     private var currentUser: UserModel?
 
@@ -16,13 +17,16 @@ class AccountTableViewController: UITableViewController {
     @IBOutlet weak var unitLabel: UILabel!
     @IBOutlet weak var emailLabel: UILabel!
     @IBOutlet weak var phoneNumberLabel: UILabel!
-
+    
+    private var currentCallNotificationType: CallNotificationType = .videoCall
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.tableView.delegate = self
-        self.tableView.dataSource = self
+        tableView.delegate = self
+        tableView.dataSource = self
         SVProgressHUD.setDefaultStyle(.light)
         getUserInfo()
+        currentCallNotificationType = CallNotificationTypeManager.shared.getCurrentCallNotificationType()
     }
 
     func getUserInfo() {
@@ -37,45 +41,82 @@ class AccountTableViewController: UITableViewController {
         unitLabel.text = ""
     }
     
-    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        if indexPath.section == 1 && indexPath.row == 0 {
-            SVProgressHUD.show()
-            
-            let tenants = BMXUser.shared.getTenants()
-            
-            let dispatchGroup = DispatchGroup()
-            
-            for tenant in tenants {
-                let key = "webhook-\(tenant.id)"
-                if let webhookId = UserDefaults.standard.string(forKey: key) {
-                    dispatchGroup.enter()
-                    
-                    BMXCoreKit.shared.unregisterWebhook(withTenantId: tenant.id, webhookId: webhookId) { result in
-                        switch result {
-                        case .success:
-                            print("remove webhook for key: \(key)")
-                            UserDefaults.standard.removeObject(forKey: key)
-                        case .failure(let error):
-                            print(error.localizedDescription)
-                        }
-                        
-                        dispatchGroup.leave()
-                    }
-                }
-            }
-            
-            dispatchGroup.notify(queue: .main) {
-                print("Finished all requests.")
-                
-                BMXCoreKit.shared.logoutUser()
-                SVProgressHUD.dismiss()
-                
-                let stb = UIStoryboard(name: "Main", bundle: nil)
-                let loginViewController = stb.instantiateViewController(withIdentifier: "LoginViewController")
-                loginViewController.modalPresentationStyle = .fullScreen
-                self.present(loginViewController, animated: true, completion: nil)
-            }
+    override func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        if indexPath.section == 1 && indexPath.row == currentCallNotificationType.rawValue {
+            cell.accessoryType = .checkmark
+        } else {
+            cell.accessoryType = .none
         }
     }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if indexPath.section == 1 {
+            guard let indexPaths = tableView.indexPathsForVisibleRows else {
+                return
+            }
+            
+            let cell = tableView.cellForRow(at: indexPath)
+            
+            for index in indexPaths {
+                if index == indexPath {
+                    currentCallNotificationType = CallNotificationType(rawValue: indexPath.row)!
+                    cell?.accessoryType = .checkmark
+                    saveSelectedCallNotificationType(indexPath.row)
 
+                } else {
+                    cell?.accessoryType = .none
+                }
+            }
+            tableView.reloadData()
+        } else if indexPath.section == 2 {
+            logout()
+        }
+    }
+    
+    private func logout() {
+        SVProgressHUD.show()
+        
+        let tenants = BMXUser.shared.getTenants()
+        
+        let dispatchGroup = DispatchGroup()
+        
+        for tenant in tenants {
+            let key = "webhook-\(tenant.id)"
+            if let webhookId = UserDefaults.standard.string(forKey: key) {
+                dispatchGroup.enter()
+                
+                BMXCoreKit.shared.unregisterWebhook(withTenantId: tenant.id, webhookId: webhookId) { result in
+                    switch result {
+                    case .success:
+                        print("remove webhook for key: \(key)")
+                        UserDefaults.standard.removeObject(forKey: key)
+                    case .failure(let error):
+                        print(error.localizedDescription)
+                    }
+                    
+                    dispatchGroup.leave()
+                }
+            }
+        }
+        
+        dispatchGroup.notify(queue: .main) {
+            print("Finished all requests.")
+            
+            BMXCoreKit.shared.logoutUser()
+            SVProgressHUD.dismiss()
+            
+            let stb = UIStoryboard(name: "Main", bundle: nil)
+            let loginViewController = stb.instantiateViewController(withIdentifier: "LoginViewController")
+            loginViewController.modalPresentationStyle = .fullScreen
+            self.present(loginViewController, animated: true, completion: nil)
+        }
+    }
+    
+    private func saveSelectedCallNotificationType(_ selectedCallNotificationType: Int) {
+        guard let callNotificationType = CallNotificationType(rawValue: selectedCallNotificationType) else {
+            return
+        }
+        
+        CallNotificationTypeManager.shared.saveSelectedCallNotificationType(callNotificationType)
+    }
 }
---
+++ ButterflyMX Demo/Logic/CallNotificationTypeManager.swift
@@ -0,0 +1,31 @@
+//
+//  CallNotificationTypeManager.swift
+//  ButterflyMX Demo
+//
+//  Created by Yingtao Guo on 11/18/21.
+//  Copyright © 2021 Taras Markevych. All rights reserved.
+//
+
+import Foundation
+
+enum CallNotificationType: Int {
+    case videoCall
+    case pushNotification
+}
+
+let callNotificationKey = "callNotificationKey"
+
+class CallNotificationTypeManager {
+    static let shared = CallNotificationTypeManager()
+    
+    private init() {}
+    
+    func saveSelectedCallNotificationType(_ callNotificationType: CallNotificationType) {
+        UserDefaults.standard.set(callNotificationType.rawValue, forKey: callNotificationKey)
+    }
+    
+    func getCurrentCallNotificationType() -> CallNotificationType {
+        let callNotficationType = UserDefaults.standard.integer(forKey: callNotificationKey)        
+        return CallNotificationType(rawValue: callNotficationType) ?? .videoCall
+    }
+}
